211f166887ef490ce088e7d7e5c681ba7d299a2a
Ali Kingravi <alikingravi@hotmail.com>
2025-09-14 17:39:42 +0100

    removed unused env url

--- lib/env.ts
+++ lib/env.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  NEXT_PUBLIC_API_URL: z.string(),
   NEXT_PUBLIC_COMPARISON_API_URL: z.string(),
   NEXT_PUBLIC_CURRENCY_API_URL: z.string(),
 });
